775d5000aa8c52714c43c1b9bffb707f41c47d74
Emil Berg <emil.berg1991@gmail.com>
2020-12-12 18:39:11 +0100

    Cleanup in progress bar

--- lib/Progress-CPP/include/Progress-CPP/ProgressBar.hpp
+++ lib/Progress-CPP/include/Progress-CPP/ProgressBar.hpp
@@ -1,35 +1,38 @@
-
-#pragma once
+#ifndef LIB_PROGRESS_CPP_INCLUDE_PROGRESS_CPP_PROGRESS_BAR_H_
+#define LIB_PROGRESS_CPP_INCLUDE_PROGRESS_CPP_PROGRESS_BAR_H_
 
 #include <chrono>
+#include <cstdint>
 
 namespace progresscpp {
 
 class ProgressBar {
    public:
-    ProgressBar(size_t total, size_t width, char complete, char incomplete)
+    ProgressBar(uint64_t total, unsigned int width, char complete, char incomplete)
         : total_ticks{total}, bar_width{width}, complete_char{complete}, incomplete_char{incomplete} {}
 
-    ProgressBar(size_t total, size_t width) : total_ticks{total}, bar_width{width} {}
+    ProgressBar(uint64_t total, unsigned int width) : total_ticks{total}, bar_width{width} {}
 
-    size_t operator++() { return ++ticks; }
+    uint64_t operator++() { return ++ticks; }
 
-    void operator+=(size_t amount) { ticks += amount; }
+    void operator+=(uint64_t amount) { ticks += amount; }
 
-    size_t get_ticks() const { return ticks; }
+    uint64_t get_ticks() const { return ticks; }
 
     void display() const;
 
     void done();
 
    private:
-    size_t ticks = 0;
-
-    const size_t                                total_ticks;
+    const uint64_t                              total_ticks;
     const unsigned int                          bar_width;
     const char                                  complete_char   = '=';
     const char                                  incomplete_char = ' ';
     const std::chrono::steady_clock::time_point start_time      = std::chrono::steady_clock::now();
+
+    uint64_t ticks = 0;
 };
 
 }  // namespace progresscpp
+
+#endif
